49cbc65e8e8c64cb9ce6aaaf945fd45497f0143d
pavan1997 <pawanvicky1997@gmail.com>
2022-03-09 23:51:44 +0530

    string someone changed inTestgreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "HI . How is ur course going on ";
+    String someone = "HI . How is ur course going on everyday";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
